3d6cff0268a7db832bbdec7e980ddd5de033a3e8
Maliarets <MMaliarets@DBDC.luxoft.com>
2018-09-22 10:31:33 +0200

    migrate to postgres

--- mariamalia/settings.py
+++ mariamalia/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'mariamalia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mariamalia',
+        'USER': 'marie0901',
+        'PASSWORD': 'luka0901',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
